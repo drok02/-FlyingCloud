cb6ec94578a8b6dd47409f01feea9ae4c262c755
drok02 <drok02@naver.com>
2022-09-07 17:17:54 +0900

    append cli text write func

--- openstack/DR/freezer_cli.py
+++ openstack/DR/freezer_cli.py
@@ -5,10 +5,11 @@ import paramiko
 address = "192.168.0.118"
 tenet_id = "30ea542f8d2740459116a43ffa82eb3f"
 serverName = "test"
-serverPw= "0000"
+serverPw = "0000"
+
 
 class AccountView():
- 
+
     # 토큰 받아오기
     def token(self):
         # data2 = json.loads(request.body)
@@ -30,20 +31,29 @@ class AccountView():
                     }
                 }
             }
-        }  
+        }
 
         # Openstack keystone token 발급
-        auth_res = requests.post("http://"+address+"/identity/v3/auth/tokens",
-            headers = {'content-type' : 'application/json'},
-            data = json.dumps(token_payload))
-        #발급받은 token 출력
+        auth_res = requests.post("http://" + address + "/identity/v3/auth/tokens",
+                                 headers={'content-type': 'application/json'},
+                                 data=json.dumps(token_payload))
+        # 발급받은 token 출력
         admin_token = auth_res.headers["X-Subject-Token"]
-        print("token : \n",admin_token)
+        print("token : \n", admin_token)
         return admin_token
 
-    def readTxtFile(self,fileNm):
-        file = open(fileNm+".txt", "r", encoding="UTF-8")
-        
+    def writeTxtFile(self, fileNm, instanceid, ):
+        file = open(fileNm + ".txt", "w", encoding="UTF-8")
+        file.write('source /opt/stack/devstack/admin-openrc.sh')
+        file.write('\nfreezer-agent --action backup --nova-inst-id ')
+        file.write(instanceid)
+        file.write(
+            ' --storage local --container /home/test/backup0812_7 --backup-name backup0812_file_7 --mode nova --engine nova --no-incremental true --log-file backup0812_7.log')
+        file.close()
+
+    def readTxtFile(self, fileNm):
+        file = open(fileNm + ".txt", "r", encoding="UTF-8")
+
         data = []
         while (1):
             line = file.readline()
@@ -60,8 +70,8 @@ class AccountView():
 
         return data
 
-    #인스턴스 생성 
-    def create_backup(self):
+    # 백업 생성
+    def create_backup(self, instanceid):
         cli = paramiko.SSHClient()
         cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
 
@@ -71,16 +81,16 @@ class AccountView():
 
         cli.connect(server, port=22, username=user, password=pwd)
 
-
-        # # 3 try 
-        commandLines = self.readTxtFile("./freezercli") # 메모장 파일에 적어놨던 명령어 텍스트 읽어옴
+        self.writeTxtFile("./freezer_backup_test_0907.txt", instanceid)
+        # # 3 try
+        commandLines = self.readTxtFile("./freezer_backup_cli.txt")  # 메모장 파일에 적어놨던 명령어 텍스트 읽어옴
         print(commandLines)
 
-        stdin, stdout, stderr = cli.exec_command(";".join(commandLines)) # 명령어 실행
-        lines = stdout.readlines() # 실행한 명령어에 대한 결과 텍스트
+        stdin, stdout, stderr = cli.exec_command(";".join(commandLines))  # 명령어 실행
+        lines = stdout.readlines()  # 실행한 명령어에 대한 결과 텍스트
         resultData = ''.join(lines)
 
-        print(resultData) # 결과 확인        
+        print(resultData)  # 결과 확인
 
         # 2 try
 
@@ -120,7 +130,7 @@ class AccountView():
         # print(''.join(lines))
         # stdin, stdout, stderr = cli.exec_command("source admin-openrc.sh")
         # lines = stdout.readlines()
-        # print(''.join(lines))                
+        # print(''.join(lines))
         # stdin, stdout, stderr = cli.exec_command("freezer-agent --action backup --nova-inst-id 4aff323c-089a-4f1d-a95f-fd045172e222 --storage local --container home/test/backup0812_4 --backup-name backup0812_file_4 --mode nova --engine nova --no-incremental true --log-file backup0812_4.log")
         # lines = stdout.readlines()
         # print(''.join(lines))
@@ -137,25 +147,24 @@ class AccountView():
 
         cli.connect(server, port=22, username=user, password=pwd)
 
-
         # # 3 try
 
-
-
-        commandLines = self.readTxtFile("./freezer_restore_cli") # 메모장 파일에 적어놨던 명령어 텍스트 읽어옴
+        commandLines = self.readTxtFile("./freezer_restore_cli")  # 메모장 파일에 적어놨던 명령어 텍스트 읽어옴
         print(commandLines)
 
-        stdin, stdout, stderr = cli.exec_command(";".join(commandLines)) # 명령어 실행
-        lines = stdout.readlines() # 실행한 명령어에 대한 결과 텍스트
+        stdin, stdout, stderr = cli.exec_command(";".join(commandLines))  # 명령어 실행
+        lines = stdout.readlines()  # 실행한 명령어에 대한 결과 텍스트
         resultData = ''.join(lines)
 
-        print(resultData) # 결과 확인     
+        print(resultData)  # 결과 확인
         cli.close()
-        
+
+
 def main():
-    f=AccountView()
-    f.token()
-    # f.create_instance()
+    f = AccountView()
+    # f.token()
+    # f.writeTxtFile('/Users/ibonghun/Developer/FlyingCloud/DR/testfreezer.txt','bhff323c-089a-4f1d-a95f-fd045172e222')
+    # f.create_backup('bhff323c-089a-4f1d-a95f-fd045172e222')
     # f.create_img_from_server("ubuntu_backup_test","image_backup_test")
     # admin_token = f.token()
     # user_res = requests.get("http://192.168.0.118/image/v2/images/0eb01803-788f-4461-aea5-737050c05148/file",
@@ -165,4 +174,6 @@ def main():
     # f.write(user_res.content)
     # f.close
     # f.restore()
+
+
 main()
