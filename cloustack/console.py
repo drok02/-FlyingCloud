588aab1c38d5044131a2834d002024d3e6dcf1bc
drok02 <drok02@naver.com>
2022-09-15 04:57:00 +0900

    til 0914:0223

--- cloustack/console.py
+++ cloustack/console.py
@@ -7,6 +7,7 @@ import sys,os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
 import urls as key
 import signature
+from bs4 import BeautifulSoup
 
 def console(vmid):
     baseurl=key.baseurl
@@ -18,12 +19,11 @@ def console(vmid):
     request= {"vm":vmid,"apiKey": apiKey, "response": "json", "cmd": "access"}
 
     response=signature.requestsig(baseurl,secretkey,request)
-    print(response)
-    # print(json.dumps(res,indent=2))
-    # print(req)
-    # jsonData=json.loads(response)
-    # print(jsonData["listserviceofferingsresponse"]["serviceoffering"][0]["id"])
-    # return jsonData["listserviceofferingsresponse"]["serviceoffering"][0]["id"]
-#vm=29603248-6d8a-4582-aa9a-4d1bfb4d7714&apikey=3NRrdrhDTwggQ_oQny11dD39-XRWJxCd0dh2xqtMNShrz_jb4ZdhHtmRh7NYiOfRzLNwPcBVAfT9FHh9v96vzg&response=json&signature=u4c7QZNQNcN+2s3fhRNSHTyl7+Q=
-#http://211.197.83.186:8080/client/api?apiKey=YWYjj6hm1w3SE802vm21xfWl4gFq-K0WuQNkCFx7b7hUtfkqmgXzFLZ_G9k8xDST4gU6U-Fyh_gN9Ft17Xcdww&response=json&cmd=access&vm=c80d6745-7714-4bf7-81ad-c37ff0d71fed&signature=crRjFnecqtHWPfcZnfUTsc4RUks%3D
-# console("c80d6745-7714-4bf7-81ad-c37ff0d71fed")
+
+    htmlData = BeautifulSoup(response,features="html.parser")
+    console_url_body=htmlData.html.frameset.frame['src']
+    console_url="http:"+console_url_body
+    print("Console URL is : \n",console_url)
+    return console_url
+
+console("219313e8-3588-4b5e-a299-37ce35463035")
